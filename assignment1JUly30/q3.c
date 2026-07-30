871ce50aba7382f164ff746663ad405d1702c79a
AnkanInfinte <ankankundu907@gmail.com>
2026-07-30 15:40:04 +0000

    Implemented Bubble Sort to Arrange marks in an array in ascending order Assignment 1 dated 30/07/2026

---
+++ assignment1JUly30/q3.c
@@ -0,0 +1,22 @@
+/*
+30/07/2026
+
+A class Teacher has a list of marks of 10 students in a random fashion .
+All the marks are whole numebrs.Before pusblishing the result the class teacher wants to
+arrange the marks in an order so that the rank of the students can easily be detected .
+Use a method to order the marks in ascending order.
+Which methodology you use here implement the method using C programming languague .
+(We can use any sorting techniques but here we use Bubble sort)
+
+Sample Input : {99,85,58,71,46,80,78,65,49,55}
+*/
+#include<stdio.h>
+
+int swap(int a , int b){
+    
+}
+void bubbleSort(int arr[],int size){
+    for(int i=0 ; i< size ; i++){
+
+    }
+}
